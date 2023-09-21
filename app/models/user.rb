67efab52d2411b372ccadf73bbd01389b2685403
abbie <supermercadoabbie@gmail.com>
2023-09-22 01:40:59 +0800

    -- feat: setup the auth with devise + jwt

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  include Devise::JWT::RevocationStrategies::JTIMatcher
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, 
+         :jwt_authenticatable, jwt_revocation_strategy: self
+
+  # def verify_password(password, password_confirmation)
+  #   if password == password_confirmation
+  #     self.password = password
+  #   else
+  #     errors.add(:password, "doesn't match")
+  #     false
+  #   end
+  # end
+end
