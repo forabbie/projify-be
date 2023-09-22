1d0659d6ba30ed9013f772d10ac41415b2256529
Abbie <90335525+forabbie@users.noreply.github.com>
2023-09-22 21:40:26 +0800

    -- feat: current user endpoint (#3)

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      # get '/user/index'
+      get '/users/current', to: 'user#logged_user'
+    end
+  end
   devise_for :users, path: '', path_names: {
-    sign_in: '/auth/signin',
-    sign_out: '/auth/signout',
-    registration: 'signup'
+    sign_in: '/api/v1/auth/signin',
+    sign_out: '/api/v1/auth/signout',
+    registration: '/api/v1/signup'
   },
   controllers: {
     sessions: 'users/sessions',
