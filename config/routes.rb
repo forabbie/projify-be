bc2056b9759b9552c3984889d3365478d80ef2c0
abbie <supermercadoabbie@gmail.com>
2023-10-15 07:32:14 +0800

    -- feat: retreiving of workspace with members/projects details

--- config/routes.rb
+++ config/routes.rb
@@ -8,23 +8,21 @@ Rails.application.routes.draw do
       get '/invitations/validate' => 'invitations#validate_invitation'
       get '/projects/all' => 'projects#all_projects'
       post '/invitations/accept' => 'invitations#accept_invitation'
+      post '/invitations/decline' => 'invitations#decline_invitation'
       resources :workspaces do
+        get '/:data_type', to: 'workspaces#workspace_data', constraints: { data_type: /(members|projects)/ }
         resources :projects, only: [:create, :index] do
-          # get 'users', on: :user
           post 'add_user', on: :member
         end
+        resources :invitations, only: [:create, :index]                  
         post '/invitations/send' => 'invitations#send_invitation'
-        resources :invitations, only: [:create, :index] do
-          # post 'accept' => 'invitations#accept_invitation', on: :member
-          post 'decline' => 'invitations#decline_invitation', on: :member
-        end                       
       end
     end
   end
   devise_for :users, path: '', path_names: {
-    sign_in: '/api/v1/auth/signin',
-    sign_out: '/api/v1/auth/signout',
-    registration: '/api/v1/signup'
+    sign_in: '/auth/signin',
+    sign_out: '/auth/signout',
+    registration: '/signup'
   },
   controllers: {
     sessions: 'users/sessions',
